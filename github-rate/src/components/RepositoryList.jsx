4daff0d549d2e226a4acadba71559d444f58d066
dsvivass <dsvivass@unal.edu.co>
2022-07-17 19:21:39 -0500

    Yup and fetch implemented

--- github-rate/src/components/RepositoryList.jsx
+++ github-rate/src/components/RepositoryList.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlatList, Text } from 'react-native'
-import repositories from '../data/repositories'
 import RepositoryItem from './RepositoryItem'
 
 function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    const fetchRepositories = async () => {
+        const response = await fetch('http://localhost:5000/api/repositories')
+        const json = await response.json()
+        setRepositories(json)
+    }
+
+    useEffect(() => {
+        fetchRepositories()
+    }, [])
+
+    const repositoriesNodes = repositories ? repositories.edge.map(edge => edge.node) : []
+
     return (
         <FlatList
             data={repositories}
             ItemSeparatorComponent={() => <Text />}
             renderItem={({ item: repo }) => (
-                <RepositoryItem {...repo}/>
+                <RepositoryItem {...repo} />
             )}
         />
     )
